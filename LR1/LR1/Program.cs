84a9a294fb3b55dde264eac6ce567239ff750a99
Asus <dayana.pozova@yandex.ru>
2025-09-25 16:26:59 +0300

    запросила у пользователя количество элементов в массиве и преобразовала эту строку в число

---
+++ LR1/LR1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите количество элементов массива: ");
+            int Lenght=Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
