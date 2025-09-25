975934ed6df5a4b6d4f7d6c5ac2b104552850877
Asus <dayana.pozova@yandex.ru>
2025-09-25 16:59:11 +0300

    задала переменные и нашла среднее значение

--- LR1/LR1/Program.cs
+++ LR1/LR1/Program.cs
@@ -18,7 +18,14 @@ namespace LR1
                 Console.Write($"Элемент [{index}]: ");
                 Array[index] = Convert.ToInt32(Console.ReadLine());
             }
-
+            int sum = 0;
+            double average = 0.0;
+            int count = 0;
+            foreach (int element in Array)
+            {
+                sum += element;
+            }
+            average = sum / Lenght;
         }
     }
 }
