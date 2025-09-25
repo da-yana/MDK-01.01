9732d44f34bf10cf06a60705c9d7a2c6942a3647
Asus <dayana.pozova@yandex.ru>
2025-09-25 17:30:25 +0300

    создала массив размером 1990 элементов и вызвала переменную со значением 10

---
+++ LR2/LR2/Program.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] Array = new int[1990];
+            int n = 10;
+        }
+    }
+}
