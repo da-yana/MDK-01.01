71a7f258c78aaace44c01c3e5e299e6e2c77abe5
Kseniapav <kseniapavlova5916@gmail.com>
2025-10-25 12:08:48 +0300

    Добавили три ключа со своими значениями

--- Dictionaru/Dictionaru/Program.cs
+++ Dictionaru/Dictionaru/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,12 @@ namespace Dictionaru
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>(); 
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            dictionary.Add("cat", new List<string>() { "Black", "white", "blue"});
+            dictionary.Add("dog", new List<string>() {"Brown", "yellow" });
+            dictionary.Add("bear", new List<string>() {"Dark brown" });
+
         }
+        
     }
 }
