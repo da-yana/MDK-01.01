713e9742fecf8ca3d46d0de18fea394f6c538a4c
sellasouls666 <pgbc303@gmail.com>
2025-12-03 09:06:42 +0300

    Создан класс HourlySalaryEmployee и добавлены поля hourlyRate(почасовая ставка), regularHours(отработанные часы) и overtimeHours(сверхурочные часы)

---
+++ tasks classes/task 1.1/task 1.1/HourlySalaryEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1._1
+{
+    internal class HourlySalaryEmployee 
+    {
+        private decimal hourlyRate_;
+        private int regularHours_;
+        private int overtimeHours_;
+    }
+}
