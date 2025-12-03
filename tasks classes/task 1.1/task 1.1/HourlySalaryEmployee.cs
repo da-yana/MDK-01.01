3e2092855b4e920dc063dba6426ae0cb89454420
sellasouls666 <pgbc303@gmail.com>
2025-12-03 09:13:52 +0300

    Добавлено наследие Employee, а также создан метод CalculateSalary(), в котором идёт расчет за обычные часы - стоимость одного часа умножается на количество всех отработанных часов

--- tasks classes/task 1.1/task 1.1/HourlySalaryEmployee.cs
+++ tasks classes/task 1.1/task 1.1/HourlySalaryEmployee.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace task_1._1
 {
-    internal class HourlySalaryEmployee 
+    internal class HourlySalaryEmployee : Employee
     {
         private decimal hourlyRate_;
         private int regularHours_;
         private int overtimeHours_;
+
+        public void SetHourlyRate(decimal rate)
+        {
+            hourlyRate_ = rate;
+        }
+
+        public decimal GetHourlyRate()
+        {
+            return hourlyRate_;
+        }
+        public void SetRegularHours(int hours)
+        {
+            regularHours_ = hours;
+        }
+
+        public int GetRegularHours()
+        {
+            return regularHours_;
+        }
+        public void SetOvertimeHours(int hours)
+        {
+            overtimeHours_ = hours;
+        }
+
+        public int GetOvertimeHours()
+        {
+            return overtimeHours_;
+        }
+        public override decimal CalculateSalary()
+        {
+            decimal regularSalary = hourlyRate_ * regularHours_;
+           
+        }
     }
 }
