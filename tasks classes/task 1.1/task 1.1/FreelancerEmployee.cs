6c29028f1c20d7bac754e11ff86228ced2fc8a9d
sellasouls666 <pgbc303@gmail.com>
2025-12-03 09:33:50 +0300

    реализован метод CalculateSalary() и в нем добавлен расчёт налога - доход умножается на налоговую ставку

--- tasks classes/task 1.1/task 1.1/FreelancerEmployee.cs
+++ tasks classes/task 1.1/task 1.1/FreelancerEmployee.cs
@@ -10,5 +10,27 @@ namespace task_1._1
     {
         private decimal projectPayment_;
         private decimal taxRate_;
+
+        public void SetProjectPayment(decimal payment)
+        {
+            projectPayment_ = payment;
+        }
+
+        public decimal GetProjectPayment()
+        {
+            return projectPayment_;
+        }
+        public void SetTaxRate(decimal rate)
+        {
+            taxRate_ = rate;
+        }
+        public decimal GetTaxRate()
+        {
+            return taxRate_;
+        }
+        public override decimal CalculateSalary()
+        {
+            decimal taxAmount = projectPayment_ * taxRate_;
+        }
     }
 }
