b211400fd252217c252374b760dbf9d5feb9ffbc
sellasouls666 <pgbc303@gmail.com>
2025-12-03 09:25:41 +0300

    создан класс FreelancerEmployee с наследием Employee и добавлены поля projectPayment(оплата за проект) и taxRate(ставка ндфл)

---
+++ tasks classes/task 1.1/task 1.1/FreelancerEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1._1
+{
+    internal class FreelancerEmployee : FreelancerEmployee
+    {
+        private decimal projectPayment_;
+        private decimal taxRate_;
+    }
+}
