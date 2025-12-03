94571dd43c7c38f441f3074eae3f0b8bd467a972
sellasouls666 <pgbc303@gmail.com>
2025-12-03 09:01:26 +0300

    создан класс FixedSalaryEmployee с наследованием : Employee, в котором присутствует поле monthlySalary_ А также созданы метод CalculateSalary() для расчёта фиксированной месяной зарплаты

---
+++ tasks classes/task 1.1/task 1.1/FixedSalaryEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1._1
+{
+    internal class FixedSalaryEmployee : Employee
+    {
+        private decimal monthlySalary_;
+
+        public void SetMonthlySalary(decimal salary)
+        {
+            monthlySalary_ = salary;
+        }
+
+        public decimal GetMonthlySalary()
+        {
+            return monthlySalary_;
+        }
+
+        public override decimal CalculateSalary()
+        {
+            return monthlySalary_;
+        }
+    }
+}
+
