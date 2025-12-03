8f0509967ffcd1667508059c55ddea403ab483f4
sellasouls666 <pgbc303@gmail.com>
2025-12-03 08:48:26 +0300

    класс переделан на абстрактный, созданы методы для работы для работы с полями, а также создан абстрактный метод CalculateSalary()

--- tasks classes/task 1.1/task 1.1/Employee.cs
+++ tasks classes/task 1.1/task 1.1/Employee.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace task_1._1
 {
-    internal class Employee
+    internal abstract class Employee
     {
         public int Id_;
         public string FullName_;
         public string email_;
 
+        public void SetId(int Id)
+        {
+            Id_ = Id;
+        }
+        public int GetId()
+        {
+            return Id_;
+        }
+        public void SetFullName(string FullName)
+        {
+            FullName_ = FullName;
+        }
+        public string GetFullName()
+        {
+            return FullName_;
+        }
+        public void SetEmail(string email)
+        {
+            email_ = email;
+        }
+
+        public string GetEmail()
+        {
+            return email_;
+        }
+        public abstract decimal CalculateSalary();
     }
 }
+
