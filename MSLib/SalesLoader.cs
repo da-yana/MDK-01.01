01d0e89b150156b74abcc1478f1818cec993b226
st56 <st56@WS56-03>
2026-04-03 11:19:04 +0300

    сделали небольшую программу для дальнейшего интеграционного тестирования.

---
+++ MSLib/SalesLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSLib
+{
+    public class SalesLoader
+    {
+        List <Product> Sales = new List <Product> ();
+        public List <Product> Load (string path)
+        {
+            List <Product> products = new List<Product> ();
+            StreamReader reader = new StreamReader (path);  
+            reader.ReadLine ();
+            string line; 
+            while ((line = reader.ReadLine ()) != null)
+            {
+                string[] ProductInf = line.Split (' ');
+                string name_ = ProductInf[0];
+                int price = Convert.ToInt32(ProductInf[1]);
+                int quantity = Convert.ToInt32(ProductInf[2]);
+                Product product = new Product (name_, price, quantity);
+                Sales.Add(product); 
+            }
+            return Sales;
+        }
+    }
+}
---
+++ MSTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSLib;
+using System;
+using System.Collections.Generic;
+
+namespace MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = "D:\\п30\\Буренина\\репреп\\MSTest";
+           SalesLoader loader = new SalesLoader();
+            List<Product> at = loader.Load(path);
+
+            List<Product> aspect = new List<Product>;
+            aspect.Add(new Product ("Печенье"; 100; 5));
+        }
+    }
+}
